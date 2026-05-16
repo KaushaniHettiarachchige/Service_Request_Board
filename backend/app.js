9377877873b7837a29124ee313ed39e247281c4e
Kaushani Hettiarachchige <kaushanihettiarachchige@gmail.com>
2026-05-17 00:06:07 +0530

    Prepare project for deployment

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,21 @@ const { notFound, errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.CLIENT_URL,
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
 
